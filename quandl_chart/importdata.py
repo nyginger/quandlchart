cd8b5861997ae8615605f6561035fd2524d23b6d
nyginger <whatsup2@gmail.com>
2018-05-16 11:21:47 +0900

    Update importdata.py

--- quandl_chart/importdata.py
+++ quandl_chart/importdata.py
@@ -4,46 +4,16 @@ import csv
 from .models import API_keys, Items_table, Indicators,Countries
 #from .forms import ItemForm
 
-'''to use mongodb
-from pymongo import MongoClient
-MONGO_HOST= 'mongodb://127.0.0.1:27017' 
-client = MongoClient(MONGO_HOST)
-db = client.local'''
 
 def importquandl(request):
-    '''if request.method=='POST':
-        form=  ItemForm(request.POST)
-        form.save()
-        
-    key=API_keys.object.all()
 
-    url=''
-    result=requests.get(url).json()
-    print(r.text)
-    item1=result['item']
-
-    items={
-        'item1': result['item1'],
-        'item2': result['item2']
-    }
-    context={'items':items}
-    return render(request, 'quandl_chart/index.html', context)'''
     create_list=[]
     dataset=request.GET['dataset'].upper()
     with open('quandl_chart/dataset/{}-datasets-codes.csv'.format(dataset),'r') as file:
         result=csv.reader(file, delimiter=',')
         print(result)
         for row in result:
-            '''' to use mongodb
-            dt_dict={}
-            dt_dict['name']=row[0]
-            dt_dict['dataset']=row[1]
-            dt_dict['symbol']=row[2]
-            dt_dict['description']=row[3]
 
-            create_list.append(dt_dict)
-            
-        db.quandl.insert_many(create_list) '''
             if len(row)>=7:
                 item=Items_table(name=row[0],dataset=row[1], symbol=row[2], description=row[3], category=row[4], subcategory=row[5], country=row[6],ccode=row[7])
             elif len(row)>=5:
